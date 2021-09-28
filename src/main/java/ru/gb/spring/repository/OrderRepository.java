fd9b752fef666cd20de1754e0c3ff3b71dc63ba9
Renat <ren_tirs@mail.ru>
2021-09-28 13:02:20 +0300

    Lesson9

--- src/main/java/ru/gb/spring/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.gb.spring.domain.Order;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-}
